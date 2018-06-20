321f70099e3627cf334dce453fb1402106e08ac9
Taina Simoes <taina.simoes.santos@gmail.com>
2018-06-20 18:56:56 -0300

    init modal

--- components/Head.js
+++ components/Head.js
@@ -23,6 +23,9 @@ const Head = (props) => {
       <meta property="og:image" content={props.image} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+
+      <link rel="stylesheet" href="/static/css/slick.css" />
+
       <link rel="stylesheet" href="/_next/static/style.css" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
     </NextHead>
